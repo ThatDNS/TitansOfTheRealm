86a048cd107337efab83f33a5b318c7e09ddc48b
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-03-11 19:24:35 -0400

    Add physics to hands (#7)

---
+++ Assets/VRtest/Oculus Hands Physics/Scripts/HandPresencePhysics.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPresencePhysics : MonoBehaviour
+{
+    [Tooltip("VR Hand transform")]
+    public Transform target;
+
+    private Rigidbody rb;
+    private Collider[] handColliders;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var collider in handColliders)
+        {
+            collider.enabled = true;
+        }
+    }
+
+    public void EnableHandColliderWithDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var collider in handColliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        Quaternion rotDiff = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotDiff.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDiffInDegree = angleInDegree * rotationAxis;
+        rb.angularVelocity = (rotationDiffInDegree * Mathf.Deg2Rad) / Time.fixedDeltaTime;
+    }
+}
