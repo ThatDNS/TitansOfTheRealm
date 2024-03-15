23875c7f5b545011cb4a719d652ef26fb5913699
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-03-14 20:19:32 -0400

    Its not working :'(

--- Assets/Game/Scripts/TallTree.cs
+++ Assets/Game/Scripts/TallTree.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : NetworkBehaviour
+public class TallTree : NetworkBehaviour
 {
     private Rigidbody rb;
 
