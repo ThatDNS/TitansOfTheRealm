bf07a26a747f0c5579770a1b510f935c74450446
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-03-28 20:53:06 -0400

    idk what this is about -- some changes

--- Assets/StartManager.cs
+++ Assets/StartManager.cs
@@ -3,11 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StartManager : MonoBehaviour
 {
+    [SerializeField] GameObject vrHardwareRig;
+    [SerializeField] GameObject pcHardwareRig;
     [SerializeField] ConnectionManager connectionManager;
+    [SerializeField] GameObject pcJoinCanvas;
 
     public bool gameStarted = false;
+
+    private void Start()
+    {
+        if (vrHardwareRig.activeInHierarchy)
+        {
+
+        }
+        else if (pcHardwareRig.activeInHierarchy)
+        {
+
+        }
+    }
 
     public async void JoinTheNetwork()
     {
